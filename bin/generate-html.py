14580fc4ebe9ea904156d2987cbc0d003a8d5d9a
Timothée Floure <timothee.floure@posteo.net>
2020-04-09 13:14:34 +0200

    Handle multiple repositories in html generation, add crawler_entrypoint page

--- bin/generate-html.py
+++ bin/generate-html.py
@@ -2,9 +2,11 @@
 
 import os
 import sqlite3
+import sys
 import argparse
 import shutil
 import json
+import re
 from datetime import date
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -12,6 +14,23 @@ TEMPLATE_DIR='../templates'
 DBS_DIR='repositories'
 ASSETS_DIR='assets'
 SCM_MAINTAINER_MAPPING='pagure_owner_alias.json'
+
+class Package:
+    def __init__(self, name):
+        self.name = name
+        self.summary = "No summary specified."
+        self.description = "No description specified."
+
+        self.license = "unknown"
+        self.upstream = ""
+        self.maintainers = []
+        self.branches = {}
+
+    def set_branch(self, name, revision):
+        self.branches[name] = revision
+
+    def get_branch(self, name):
+        return self.branches[name]
 
 def save_to(path, content):
     with open(path, 'w') as fh:
@@ -26,62 +45,97 @@ def main():
 
     args = parser.parse_args()
 
-    # Initialize tamplating system / load initial data.
+    # Make sure output directory exists.
     output_dir = args.target_dir
-    db = os.path.join(DBS_DIR, "koji-primary.sqlite")
     os.makedirs(output_dir, exist_ok=True)
 
+    # Initialize templating system.
+    db = os.path.join(DBS_DIR, "koji-primary.sqlite")
     env = Environment(
             loader=PackageLoader('generate-html', TEMPLATE_DIR),
             autoescape=select_autoescape(['html'])
             )
 
+    # Load maintainer mapping (imported from dist-git).
+    # TODO: check that mapping exist / error.
     print("Loading maintainer mapping...")
     with open(SCM_MAINTAINER_MAPPING) as raw:
         maintainer_mapping = json.load(raw)
 
-    # Generate main entrypoint.
+    # Build internal package metadata structure / cache.
+    packages = {}
+    for db in os.listdir(DBS_DIR):
+        print("> Processing database file {}.".format(db))
+        pattern = re.compile('^(fedora|epel)-([\w|-]+)_(primary|filelists|other).sqlite$')
+        match = pattern.match(db)
+
+        if (not match):
+            sys.exit("Invalid object in {}: {}".format(DBS_DIR, db))
+
+        (product, branch, db_type) = pattern.findall(db)[0]
+
+        conn = sqlite3.connect(os.path.join(DBS_DIR, db))
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+
+        if db_type == "primary":
+            for raw in c.execute('SELECT * FROM packages'):
+                pkg = packages.get(raw["name"])
+                revision = "{}-{}".format(raw["version"], raw["release"])
+                release = "{}-{}".format(product, branch)
+                if pkg == None:
+                    pkg = Package(raw["name"])
+                    pkg.summary = raw["summary"]
+                    pkg.description = raw["description"]
+                    pkg.upstream = raw["url"]
+                    pkg.license = raw["rpm_license"]
+                    pkg.maintainers = maintainer_mapping["rpms"].get(pkg.name, [])
+
+                    pkg.set_branch(release, revision)
+                    packages[pkg.name] = pkg
+                else:
+                    pkg.set_branch(release, revision)
+
+
+    print(">>> {} packages have been extracted.".format(len(packages)))
+    # Generate main user entrypoint.
     print("Generating generic pages...")
-    index = env.get_template('index.html.j2')
-    index_html = index.render(date=date.today().isoformat())
-    save_to(os.path.join(output_dir, 'index.html'), index_html)
+    search = env.get_template('search.html.j2')
+    search_html = search.render(date=date.today().isoformat(), package_count=len(packages))
+    save_to(os.path.join(output_dir, 'index.html'), search_html)
 
     # Import assets.
     shutil.copytree(ASSETS_DIR, os.path.join(output_dir, 'assets'))
 
+    # Generate indexing system entrypoint.
+    crawler_entrypoint = env.get_template('crawler_entrypoint.html.j2')
+    crawler_entrypoint_html = crawler_entrypoint.render(packages=packages)
+    save_to(os.path.join(output_dir, 'crawler-entrypoint.html'), crawler_entrypoint_html)
+
     # Generate package pages from Rawhide.
     print("Generating package pages...")
 
-    conn = sqlite3.connect(db)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
-
-    c.execute('SELECT COUNT(*) FROM packages')
-    package_count = c.fetchone()[0]
-    print("Found {} packages in Rawhide.".format(package_count))
-
     count = 0
-    for pkg in c.execute('SELECT * FROM packages'):
-        pkg_dir = os.path.join(output_dir, 'pkgs', pkg["name"])
+    for pkg in packages.values():
+        pkg_dir = os.path.join(output_dir, 'pkgs', pkg.name)
         os.makedirs(pkg_dir, exist_ok=True)
 
         html_path = os.path.join(pkg_dir, 'index.html')
         html_template = env.get_template('package.html.j2')
         html_content = html_template.render(
-                name=pkg["name"],
-                summary=pkg["summary"],
-                description=pkg["description"],
-                upstream=pkg["url"],
-                license=pkg["rpm_license"],
-                version=pkg["version"],
-                release=pkg["release"],
-                maintainers=maintainer_mapping["rpms"].get(pkg["name"], [])
+                name=pkg.name,
+                summary=pkg.summary,
+                description=pkg.description,
+                upstream=pkg.upstream,
+                license=pkg.license,
+                maintainers=pkg.maintainers,
+                branches=pkg.branches
                 )
         save_to(html_path, html_content)
         count += 1
 
         if (count % 100 == 0):
-            print("Processed {}/{} packages.".format(count, package_count))
+            print("Processed {}/{} packages.".format(count, len(packages)))
 
 
 if __name__ == '__main__':
