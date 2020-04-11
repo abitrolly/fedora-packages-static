d7b40bcdf9ae6b6896655df94e429f8226f249c2
Timothée Floure <timothee.floure@posteo.net>
2020-04-11 15:13:40 +0200

    Add initial support for changelog page

--- bin/generate-html.py
+++ bin/generate-html.py
@@ -7,6 +7,7 @@ import argparse
 import shutil
 import json
 import re
+from collections import defaultdict
 from datetime import date
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -16,8 +17,9 @@ ASSETS_DIR='assets'
 SCM_MAINTAINER_MAPPING='pagure_owner_alias.json'
 
 class Package:
-    def __init__(self, name):
+    def __init__(self, name, pkg_key):
         self.name = name
+        self.key = pkg_key
         self.summary = "No summary specified."
         self.description = "No description specified."
 
@@ -28,8 +30,8 @@ class Package:
         self.subpackage_of = None
         self.subpackages = []
 
-    def set_branch(self, name, revision):
-        self.branches[name] = revision
+    def set_branch(self, name, revision, changelog):
+        self.branches[name] = {"revision": revision, "changelog": changelog}
 
     def get_branch(self, name):
         return self.branches[name]
@@ -39,6 +41,13 @@ class Package:
             return self.name
         else:
             return self.subpackage_of
+
+def open_db(db):
+    conn = sqlite3.connect(os.path.join(DBS_DIR, db))
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+
+    return (conn, c)
 
 def save_to(path, content):
     with open(path, 'w') as fh:
@@ -70,11 +79,9 @@ def main():
     with open(SCM_MAINTAINER_MAPPING) as raw:
         maintainer_mapping = json.load(raw)
 
-    # Build internal package metadata structure / cache.
-    packages = {}
-
+    # Group databases files.
+    databases = {}
     for db in os.listdir(DBS_DIR):
-        print("> Processing database file {}.".format(db))
         pattern = re.compile('^(fedora|epel)-([\w|-]+)_(primary|filelists|other).sqlite$')
         match = pattern.match(db)
 
@@ -82,45 +89,66 @@ def main():
             sys.exit("Invalid object in {}: {}".format(DBS_DIR, db))
 
         (product, branch, db_type) = pattern.findall(db)[0]
+        release = "{}-{}".format(product, branch)
+        if release in databases:
+            databases[release][db_type] = db
+        else:
+            databases[release] = {db_type: db }
 
-        conn = sqlite3.connect(os.path.join(DBS_DIR, db))
-        conn.row_factory = sqlite3.Row
-        c = conn.cursor()
+    # Build internal package metadata structure / cache.
+    packages = {}
+    srpm_pattern = re.compile("^(.+)-.+-.+.src.rpm$")
+    for release in databases.keys():
+        print("> Processing database files for {}.".format(release))
 
-        srpm_pattern = re.compile("^(.+)-.+-.+.src.rpm$")
-        if db_type == "primary":
-            for raw in c.execute('SELECT * FROM packages'):
-                pkg = packages.get(raw["name"])
-                revision = "{}-{}".format(raw["version"], raw["release"])
-                release = "{}-{}".format(product, branch)
-                first_pkg_encounter = False
+        for db_type in ["primary", "filelists", "other"]:
+            if db_type not in databases[release]:
+                sys.exit("No {} database for {}.".format(db_type, release))
 
-                # Register unknown packages.
-                if pkg == None:
-                    pkg = Package(raw["name"])
-                    packages[pkg.name] = pkg
-                    first_pkg_encounter = True
+        (_, primary) = open_db(databases[release]["primary"])
+        (_, filelist) = open_db(databases[release]["filelists"])
+        (_, other) = open_db(databases[release]["other"])
 
-                # Override package metadata with rawhide (= lastest) values.
-                if first_pkg_encounter or branch == "rawhide":
-                    pkg.summary = raw["summary"]
-                    pkg.description = raw["description"]
-                    pkg.upstream = raw["url"]
-                    pkg.license = raw["rpm_license"]
-                    pkg.maintainers = maintainer_mapping["rpms"].get(pkg.name, [])
+        for raw in primary.execute('SELECT * FROM packages'):
+            pkg = packages.get(raw["name"])
+            revision = "{}-{}".format(raw["version"], raw["release"])
+            first_pkg_encounter = False
 
-                # Handle subpackage specific case.
-                (srpm_name) = srpm_pattern.findall(raw["rpm_sourcerpm"])[0]
-                if pkg.name != srpm_name:
-                    pkg.subpackage_of = srpm_name
+            # Register unknown packages.
+            if pkg == None:
+                pkg = Package(raw["name"], raw["pkgKey"])
+                packages[pkg.name] = pkg
+                first_pkg_encounter = True
 
-                # Always register branch-specific metadata.
-                pkg.set_branch(release, revision)
+            # Override package metadata with rawhide (= lastest) values.
+            if first_pkg_encounter or branch == "rawhide":
+                pkg.summary = raw["summary"]
+                pkg.description = raw["description"]
+                pkg.upstream = raw["url"]
+                pkg.license = raw["rpm_license"]
+                pkg.maintainers = maintainer_mapping["rpms"].get(pkg.name, [])
+
+            # Handle subpackage specific case.
+            (srpm_name) = srpm_pattern.findall(raw["rpm_sourcerpm"])[0]
+            if pkg.name != srpm_name:
+                pkg.subpackage_of = srpm_name
+
+            # Extract changelog from '-other' db.
+            changelog = []
+            for change in other.execute('SELECT * FROM changelog WHERE pkgKey = ?', (pkg.key,)):
+                changelog += [{
+                    "author": change["author"], # TODO: obfuscate mail addresses
+                    "date": change["date"],
+                    "change": change["changelog"]
+                    }]
+
+            # Always register branch-specific metadata.
+            pkg.set_branch(release, revision, changelog)
 
     # Set license and maintainers for subpackages. We have to wait for all
     # packages to have been processed since subpackage might have been
     # processed before its parent.
-    print("Syncing subpackages...")
+    print("Handling subpackages...")
     for pkg in packages.values():
         if pkg.subpackage_of != None:
             parent = packages.get(pkg.subpackage_of)
@@ -155,9 +183,23 @@ def main():
         html_template = env.get_template('package.html.j2')
         html_content = html_template.render(pkg=pkg)
         save_to(html_path, html_content)
-        count += 1
 
-        if (count % 100 == 0):
+        for branch in pkg.branches.keys():
+            os.makedirs(os.path.join(pkg_dir, branch), exist_ok=True)
+
+            pages = [
+                ('changelog.html', 'package-changelog.html'),
+                ('files.html', 'package-files.html')
+                ]
+            for output_filename, template in pages:
+                html_path = os.path.join(pkg_dir, branch, output_filename)
+                html_template = env.get_template(template)
+                html_content = html_template.render(pkg=pkg, branch=branch)
+                save_to(html_path, html_content)
+
+        # Simple way to display progress.
+        count += 1
+        if (count % 1000 == 0) or (count == len(packages)):
             print("Processed {}/{} packages.".format(count, len(packages)))
 
 
