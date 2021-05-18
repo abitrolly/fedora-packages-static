2c8ac984a4aae12c4d6cd377feb01866cd056083
Brendan Early <mymindstorm@evermiss.net>
2021-05-18 17:17:11 -0500

    move anacron spool directory to /etc/packages

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd /etc/packages
+rm -f nginx.pid supervisord.pid
+mkdir -p /etc/packages/anacron
 INIT_FILE=.init_complete
 if [[ ! -f "$INIT_FILE" ]]; then
   mkdir -p /var/run/supervisor
