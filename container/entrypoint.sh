17dc02a9c7e372330ba6a29091ad9544300daff3
Brendan Early <mymindstorm@evermiss.net>
2021-05-21 19:58:20 -0500

    remove extraneous mkdir

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd /etc/packages
 rm -f nginx.pid supervisord.pid
-mkdir -p /etc/packages/anacron
 INIT_FILE=.init_complete
 if [[ ! -f "$INIT_FILE" ]]; then
   mkdir -p /var/run/supervisor
