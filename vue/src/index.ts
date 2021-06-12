2c436e71371533349b69c27bda52a5638a69848c
Brendan Early <mymindstorm@evermiss.net>
2021-06-12 13:15:01 -0500

    use source package name for data

--- vue/src/index.ts
+++ vue/src/index.ts
@@ -2,12 +2,13 @@ import Vue from "vue";
 import Datagrepper from "./Datagrepper.vue";
 
 function init(): void {
-    const packageName = window.location.pathname.match(/\/pkgs\/(.+)\//);
+    const packageName = /\/pkgs\/(.+)\/.+\//.exec(window.location.pathname);
     if (!packageName) {
         console.error("Could not find package name!");
         return;
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     Vue.prototype.$package = packageName[1];
 
     new Vue({
